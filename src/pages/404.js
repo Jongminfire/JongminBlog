9cc976849fed12edcbd3b9285b5326e02dfc5fc3
Jongminfire <jongminfire@gmail.com>
2021-12-09 21:39:25 +0900

    feat: 404 Page 추가

---
+++ src/pages/404.js
@@ -0,0 +1,28 @@
+/** @jsx jsx */
+import { jsx, Heading, Link as TLink } from "theme-ui";
+import { Link } from "gatsby";
+import React from "react";
+import Layout from "../components/layout";
+import useMinimalBlogConfig from "../hooks/use-minimal-blog-config";
+import replaceSlashes from "../utils/replaceSlashes";
+
+const NotFound = () => {
+	const { basePath } = useMinimalBlogConfig();
+
+	return (
+		<Layout
+			children={
+				<div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+					<p sx={{ fontSize: [3, 3, 5], fontWeight: "bold", letterSpacing: "-2px" }}>PAGE NOT FOUND</p>
+					<span sx={{ fontSize: [1, 1, 2], fontWeight: "normal", letterSpacing: "-2px" }}>올바르지 않은 페이지입니다</span>
+					<br />
+					<TLink sx={{ fontSize: "16px" }} as={Link} activeClassName="active" to={replaceSlashes(`/${basePath}/`)}>
+						메인으로
+					</TLink>
+				</div>
+			}
+		></Layout>
+	);
+};
+
+export default NotFound;
